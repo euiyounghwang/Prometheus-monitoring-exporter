f181b362c29bc76c3b224b8f223fb492cd9ddb7e
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-10-10 14:47:50 -0500

    Update

--- Grafana-log/promtail-service.sh
+++ Grafana-log/promtail-service.sh
@@ -3,12 +3,20 @@
 LOKI_PATH=/home/devuser/monitoring/grafana-loki
 SERVICE_NAME=grafana-promtail-service
 
+#-- 
+# sudo vi /etc/rc.d/rc.local
+# sudo chmod 755 /etc/rc.local && sudo systemctl enable rc-local
+
+# sudo systemctl status rc-local.service
+# sudo service rc-local stop
+# sudo service rc-local status
+
 # See how we were called.
 case "$1" in
   start)
         # Start daemon.
         echo "Starting $SERVICE_NAME";
-        # nohup $LOKI_PATH/promtail-linux-amd64 --config.file=$LOKI_PATH/promtail-config.yaml &> /dev/null &
+        # nohup $LOKI_PATH/promtail-linux-amd64 --config.file=$LOKI_PATH/promtail-config.yml &> /dev/null &
         $LOKI_PATH/promtail-linux-amd64 --config.file=$LOKI_PATH/promtail-config.yml
         ;;
   stop)
