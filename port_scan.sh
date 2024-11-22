bc138ff5e2334326ef86e2d848f88a4670d8ff64
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-11-22 16:18:00 -0600

    Update

---
+++ port_scan.sh
@@ -0,0 +1,9 @@
+
+sudo netstat -nlp | grep :7091
+sudo netstat -nlp | grep :9090
+sudo netstat -nlp | grep :3000
+sudo netstat -nlp | grep :3001
+sudo netstat -nlp | grep :8888
+sudo netstat -nlp | grep :3100
+sudo netstat -nlp | grep :8080
+sudo netstat -nlp | grep :9000
