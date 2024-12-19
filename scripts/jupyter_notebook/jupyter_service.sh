67ed61726d31b295e537edee082366c29871a4f7
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-12-19 08:18:55 -0600

    Update

--- scripts/jupyter_notebook/jupyter_service.sh
+++ scripts/jupyter_notebook/jupyter_service.sh
@@ -35,7 +35,7 @@ case "$1" in
   stop)
         # Stop daemons.
         echo "Shutting down $SERVICE_NAME";
-        pid=`ps ax | grep -i '/jupyter' | grep -v grep | awk '{print $1}'`
+        pid=`ps ax | grep -i '/jupyter-notebook' | grep -v grep | awk '{print $1}'`
         if [ -n "$pid" ]
           then
           kill -9 $pid
@@ -49,7 +49,7 @@ case "$1" in
         $0 start
         ;;
   status)
-        pid=`ps ax | grep -i '/jupyter' | grep -v grep | awk '{print $1}'`
+        pid=`ps ax | grep -i '/jupyter-notebook' | grep -v grep | awk '{print $1}'`
         if [ -n "$pid" ]
           then
           echo "$SERVICE_NAME is Running as PID: $pid"
