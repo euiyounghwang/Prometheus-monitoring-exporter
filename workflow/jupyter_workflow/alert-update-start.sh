e1d9ec9a500f2229da4837de4b197c4ef8563eb6
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-01-03 11:58:11 -0600

    Update

--- workflow/jupyter_workflow/alert-update-start.sh
+++ workflow/jupyter_workflow/alert-update-start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -e
+ES_ALERT_UI_SERVICE_ALL_EXPORT_PATH='C://Users/euiyoung.hwang/Git_Workspace/Prometheus-monitoring-exporter/'
+#ES_ALERT_UI_SERVICE_ALL_EXPORT_PATH='/home/devuser/monitoring/jupyter_notebook/'
+SERVICE_NAME=es-alert-ui-service-all-service
 
+#PATH=$PATH:$ES_ALERT_UI_SERVICE_ALL_EXPORT_PATH/bin
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#echo $SCRIPTDIR
 cd $SCRIPTDIR
-
-PATH='C://Users/euiyoung.hwang/Git_Workspace/Prometheus-monitoring-exporter/'
 
 VENV=".venv"
 
 # Python 3.11.7 with Window
 if [ -d "$VENV/bin" ]; then
-    source $PATH/$VENV/bin/activate
+    source $ES_ALERT_UI_SERVICE_ALL_EXPORT_PATH/$VENV/bin/activate
 else
-    source $PATH/$VENV/Scripts/activate
+    source $ES_ALERT_UI_SERVICE_ALL_EXPORT_PATH/$VENV/Scripts/activate
 fi
+
 
 PROMETHEUS_HOST="dev:localhost,dev1:localhost"
 API_HOST="localhost"
@@ -32,4 +36,42 @@ export GRAFANA_DASHBOARD_URL
 export USER
 
 # gradio run $SCRIPTDIR/Alert_Update.py 
-python $SCRIPTDIR/Alert_Update.py
+# python $SCRIPTDIR/Alert_Update.py
+
+# See how we were called.
+case "$1" in
+  start)
+        # Start daemon.
+        echo "🦄 Starting $SERVICE_NAME";
+        #nohup python $SCRIPTDIR/Alert_Update.py &> /dev/null &
+        python $SCRIPTDIR/Alert_Update.py
+        ;;
+  stop)
+        # Stop daemons.
+        echo "🦄 Shutting down $SERVICE_NAME";
+        pid=`ps ax | grep -i '/Alert_Update.py' | grep -v grep | awk '{print $1}'`
+        if [ -n "$pid" ]
+          then
+          kill -9 $pid
+         else
+          echo "🦄 $SERVICE_NAME was not Running"
+        fi
+        ;;
+  restart)
+        $0 stop
+        sleep 2
+        $0 start
+        ;;
+  status)
+        pid=`ps ax | grep -i '/Alert_Update.py' | grep -v grep | awk '{print $1}'`
+        if [ -n "$pid" ]
+          then
+          echo "🦄 $SERVICE_NAME is Running as PID: $pid"
+        else
+          echo "🦄 $SERVICE_NAME is not Running"
+        fi
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|status}"
+        exit 1
+esac
