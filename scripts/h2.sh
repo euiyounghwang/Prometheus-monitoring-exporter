c4a223fe4797e6f873c00c4e884482a0dd5edfdd
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-01-10 16:40:43 -0600

    Update

--- scripts/h2.sh
+++ scripts/h2.sh
@@ -10,4 +10,5 @@ JAR_PATH="/home/devuser/monitoring/h2/bin"
 dir=$(dirname "$0")
 echo $dir
 #java -cp "$JAR_PATH/h2-2.3.232.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
-java -cp "$JAR_PATH/h2-2.3.232.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Server -webAllowOthers -tcpAllowOthers
+#java -cp "$JAR_PATH/h2-2.3.232.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Server -webAllowOthers -tcpAllowOthers
+java -cp "$JAR_PATH/h2-2.3.232.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Server -webAllowOthers -tcpAllowOthers -webExternalNames localhost
