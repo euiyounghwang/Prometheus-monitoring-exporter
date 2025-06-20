883bc69637ad3b2b77c36a869b9635214fb17a90
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-06-20 16:52:26 -0500

    Update

--- golang/db_api/repository/model.go
+++ golang/db_api/repository/model.go
@@ -1,18 +1,20 @@
 package repository
 
 // Struct fields must start with upper case letter (exported) for the JSON package to see their value.
+// The "omitempty" option specifies that the field should be omitted from the encoding if the field has an empty value, defined as false, 0, a nil pointer, a nil interface value, and any empty array, slice, map, or string.
+// example : https://www.joinc.co.kr/w/man/12/golang/MarshalStructsTheRightWay#google_vignette
 type Configuration struct {
-	AlertExcludeTime string `json:"alert_exclude_time"`
+	AlertExcludeTime string `json:"alert_exclude_time,omitempty"`
 	Test             struct {
-		MailList       string `json:"mail_list"`
-		CcList         string `json:"cc_list"`
-		SmsList        string `json:"sms_list"`
-		DevMailList    string `json:"dev_mail_list"`
-		DevSmsList     string `json:"dev_sms_list"`
-		Env            string `json:"env"`
-		ThreadInterval int    `json:"thread_interval"`
-		IsMailing      bool   `json:"is_mailing"`
-		IsSms          bool   `json:"is_sms"`
+		MailList       string `json:"mail_list,omitempty"`
+		CcList         string `json:"cc_list,omitempty"`
+		SmsList        string `json:"sms_list,omitempty"`
+		DevMailList    string `json:"dev_mail_list,omitempty"`
+		DevSmsList     string `json:"dev_sms_list,omitempty"`
+		Env            string `json:"env,omitempty"`
+		ThreadInterval int    `json:"thread_interval,omitempty"`
+		IsMailing      bool   `json:"is_mailing,omitempty"`
+		IsSms          bool   `json:"is_sms,omitempty"`
 	} `json:"test"`
 }
 
