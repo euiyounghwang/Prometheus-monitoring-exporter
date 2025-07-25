7d03eb0b28b42d33aaee5109efee560488031a1f
euiyounghwang <marieuig@gmail.com>
2025-07-24 21:46:17 -0500

    Update

--- golang/gin_framework/main.go
+++ golang/gin_framework/main.go
@@ -32,7 +32,7 @@ swag init
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 0.0.0.0:8080
+// @host localhost:8080
 // @BasePath /
 func main() {
 	r := gin.Default()
