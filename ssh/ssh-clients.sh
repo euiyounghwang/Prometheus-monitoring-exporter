55c4e83bee56294e5bfa0a603a9c1c87a57ce84d
euiyounghwang <marieuig@gmail.com>
2025-10-31 11:39:21 -0500

    Updtae

--- ssh/ssh-clients.sh
+++ ssh/ssh-clients.sh
@@ -17,5 +17,11 @@ else
     source $ROOTDIR/$VENV/Scripts/activate
 fi
 
+# ssh commands
+# sh -c 'sudo su - spark -c "./utils/sparkSubmitWMx.sh status"'
+# sh -c 'sudo ~/utils/kafkaUtil.sh status'
+# sh -c 'sudo ~/utils/connectUtil.sh status'
+# sh -c 'sudo su - elasticsearch -c "/apps/elasticsearch/latest/bin/elasticsearch -d"'
+
 # python $SCRIPTDIR/ssh-client.py --service elasticsearch --cmd "ls -l"
 python $SCRIPTDIR/ssh-clients.py --service elasticsearch --cmd "ps ax | grep -i '/elasticsearch' | grep -v grep | awk '{print \$1}'"
