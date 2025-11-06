fdc6d0a7575db72efed1f82cc3b54c28fde77225
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-11-05 18:14:00 -0600

    Update

--- standalone-tools-export-run.sh
+++ standalone-tools-export-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Activate virtualenv && run serivce
+
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTDIR
 
@@ -14,6 +14,7 @@ else
     source $VENV/Scripts/activate
 fi
 
+
 # Python 3.11.7 with Window
 # if [ -d "$ES_REDIS_CONFIGURATION_WRITE_PATH/$VENV/bin" ]; then
 #     source $ES_REDIS_CONFIGURATION_WRITE_PATH/$VENV/bin/activate
@@ -25,6 +26,10 @@ fi
 # -- Export Variable
 export GRAFANA_DASHBOARD_URL="http://localhost:3000/d/adm08055cf3lsa/es-team-dashboard?orgId=1'&'from=now-5m'&'to=now'&'refresh=5s"
 export PROMETHEUS_APPS_HOST="localhost"
+export PROMETHEUS_LOOKUP_HOST="localhost"
+export PROMETHEUS_USERNAME="test"
+export PROMETHEUS_PASSWORD="test"
+
 
 # -- standalone type
 # local
--- standalone-tools-service-export.py
+++ standalone-tools-service-export.py
@@ -13,6 +13,9 @@ import requests
 import logging
 import warnings
 import time
+import socket
+from flask import Flask, render_template
+
 
 warnings.filterwarnings("ignore")
 
@@ -28,6 +31,29 @@ logging.basicConfig(
 )
 
 
+def post_request_to_prometheus():
+    ''' request to proemtheus for the metrics'''
+    try:
+        resp = requests.get(
+                url="http://{}:9090/api/v1/query?query=xMatters_service_health_metric{{}}".format(os.getenv("PROMETHEUS_APPS_HOST"), os.getenv("PROMETHEUS_LOOKUP_HOST")), 
+                auth=(os.getenv("PROMETHEUS_USERNAME"), os.getenv("PROMETHEUS_PASSWORD")), 
+                timeout=5, 
+                verify=False
+            )
+                        
+        logging.info(f"post_request_to_prometheus")
+
+        if not (resp.status_code == 200):
+            return None
+        
+        logging.info(f"post_request_to_prometheus - {resp}, {resp.status_code}")
+
+        return resp.json()
+
+    except Exception as e:
+        logging.info(e)
+
+
 def work():
     ''' main process '''
     try:
@@ -36,18 +62,39 @@ def work():
             try:
                 logging.info("\n\n")
                 logging.info("** work func")
+
+                ''' request to proemtheus for the metrics'''
+                resp = post_request_to_prometheus()
+                if resp:
+                    logging.info(f"resp - {json.dumps(resp, indent=2)}")
+                    data = resp.get('data').get('result')[0]
+                    xMatters_value = int(data.get('value')[1])
+                    ''' Active if xMatters_value is 1, InActive if xMatters_value is 2'''
+                    if xMatters_value == 1:
+                        logging.info(f"xMatters Status - Active..")
+                    else:
+                        logging.info(f"xMatters Status - InActive..")
+                        ''' Sending alerts to the Team'''
                
             except Exception as e:
                 logging.info(f"work func : {e}")
-
                 pass
                                
-            time.sleep(10)
+            time.sleep(30)
 
     # except (KeyboardInterrupt, SystemExit) as e:
     except Exception as e:
         logging.info(e)
    
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    # return render_template('./index.html', host_name=socket.gethostname().split(".")[0], linked_port=port, service_host=env_name)
+    return {"app" : "standalone-tools-service-export.py"}
+
 
 
 if __name__ == '__main__':
@@ -56,7 +103,7 @@ if __name__ == '__main__':
     python ./standalone-tools-service-export.py
     """
     parser = argparse.ArgumentParser(description="Running this service to check the status of tools and send alerts using this script")
-    # parser.add_argument('-t', '--ts', dest='ts', default="https://localhost:9201", help='host target')
+    parser.add_argument('-t', '--ts', dest='ts', default="https://localhost:9201", help='host target')
     args = parser.parse_args()
     
         
@@ -74,6 +121,15 @@ if __name__ == '__main__':
         th1.daemon = True
         th1.start()
         T.append(th1)
+
+        ''' Expose this app to acesss'''
+        ''' Flask at first run: Do not use the development server in a production environment '''
+        ''' For deploying an application to production, one option is to use Waitress, a production WSGI server. '''
+        # app.run(host="0.0.0.0", port=int(port)-4000)
+        from waitress import serve
+        port = 9999
+        serve(app, host="0.0.0.0", port=9999)
+        logging.info(f"# Flask App's Port : {port}")
 
         for t in T:
             while t.is_alive():
