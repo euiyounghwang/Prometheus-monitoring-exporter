4ba5ba2b121a1db22bcccae03c7a210dfa4911a7
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-11-14 17:13:17 -0600

    Update

--- socket/multi_socket_server.py
+++ socket/multi_socket_server.py
@@ -4,17 +4,27 @@ from _thread import *
 import threading
 # sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), './'))
 
-from config.log_config import create_log
+# from config.log_config import create_log
 from dotenv import load_dotenv
 import binascii
 import subprocess
+import logging
 
 load_dotenv()
 
 
-logger = create_log()
-lock = threading.Lock()
+# 로깅 설정
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
+# logger = create_log()
+lock = threading.Lock()
+logger = logging.getLogger("socket-log")
 
 class SocketManager(object):
     def __init__(self):
@@ -101,7 +111,7 @@ def server_socket_start(my_socket, _port, service_client):
     ServerSocket = socket.socket()
     ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
     ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
-    ServerSocket.settimeout(10000000)
+    ServerSocket.settimeout(100000)
     
     # host = '0.0.0.0'  (Docker Container: 0.0.0.0, local test : 127.0.0.1)
     host = os.getenv("HOST", "127.0.0.1")
