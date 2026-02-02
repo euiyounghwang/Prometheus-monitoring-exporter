d6bd900e74da757c190b94c390584d771424243f
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2026-02-02 15:10:06 -0600

    Update

---
+++ RPC/golang_gRPC/client/gRPC_client.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"context"
+	pb "gRPC/pb" // 생성된 패키지 경로
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
